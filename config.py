daf2ac56d79492d67b604b355ef292939078a421
Gealor <gzazulya@gmail.com>
2026-07-09 18:01:17 +0400

    logger configuration

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+import logging
 from pathlib import Path
 
 from pydantic import BaseModel, SecretStr
@@ -8,7 +9,10 @@ BASE_DIR = Path(__file__).resolve().parent
 ENV_TEMPLATE = BASE_DIR / ".env.template"
 ENV_FILE = BASE_DIR / '.env'
 
-
+class LoggerSettings(BaseModel):
+    LOG_DEFAULT_FORMAT: str = "[%(asctime)s.%(msecs)03d] %(module)10s:%(lineno)-3d %(levelname)-7s - %(message)s"
+    level: int = logging.DEBUG
+    datefmt: str = "%Y-%m-%d %H:%M:%S"
 
 # Тестовые файлы
 class TestFiles(BaseModel):
@@ -39,6 +43,7 @@ class Settings(BaseSettings):
     FINAL_ACT_PATH: Path = TYPST_DIR / "act.pdf"
 
     test_files: TestFiles = TestFiles()
+    logger: LoggerSettings = LoggerSettings()
 
 
 settings = Settings() # type: ignore
